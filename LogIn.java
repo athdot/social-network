950488b628a4a363334c3db5636429156de36ab2
jmaduafo <87540591+jmaduafo@users.noreply.github.com>
2021-07-18 15:56:48 +0100

    Update LogIn.java

--- LogIn.java
+++ LogIn.java
@@ -40,11 +40,11 @@ public class LogIn {
     }
 
     // Checks if password and username/email entered matches the account's password and
-    // username/email
+    // username
     // Wasn't sure if I should throw an exception here 
     public void confirm(String newPassword, String newUser, String newEmail) {
-        if (password.equals(newPassword) && username.equals(newUser) &&
-                email.equals(newEmail)) {
+        if (password.equals(newPassword) && (username.equals(newUser) ||
+                email.equals(newEmail))) {
             isValid();
         } else {
             valid = false;
