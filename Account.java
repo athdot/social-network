698d7d3a995db385162bd82f60a61bda04152be8
Agolli-DrunkofGod <87540656+Agolli-DrunkofGod@users.noreply.github.com>
2021-07-19 17:53:48 -0400

    1

---
+++ Account.java
@@ -0,0 +1,185 @@
+import java.util.*;
+
+/**
+ * Account - CS180 PJ04
+ * This is a simple class to manage a user Account
+ * @author Charles Graham
+ * @version 07/17/2021
+ *
+ **/
+//ewewew
+public class Account {
+    // Basic login information
+    private String username;
+    private String password;
+
+    //A short blurb about the user 
+    private String bio;
+    private Post post;
+    private static String[] names = {"carter","language","socrates","love","orange","taper",
+            "london","trouble","sandpaper","stables","ocean","river","elephant","juice",
+            "varsace","blankets","velvet","castaways","strange","things","cards","beyonce",
+            "polar","zeus","create","pool","canvas","feather","titties","milk","box","triangle"};
+
+    //array list of comments and posts made by this account
+    private ArrayList<Post> posts;
+    private ArrayList<Comment> comments;
+
+    
+    public Account(String username, String password) {
+        //TODO: Check if user exists, and if they do output error?
+        this.username = username;
+        this.password = password; //will be encrypted from Application.java
+        posts = new ArrayList<Post> ();
+        bio = ""; //user can set the bio later
+        saveAccount();
+    }
+
+    public Account(String accountInfo) {
+        //TODO: Code later, once we have code to save a Account
+
+    }
+
+    public void saveAccount() { //save account to file
+        //TODO: Code file mgmt stuff
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public ArrayList<Post> getPosts() {
+        return posts;
+    }
+
+    public String toString() { //Outputs a user Account as a printable string
+        String lineBreak = "=-=-=-=-=-=-=-=-=-=-=--=-=";
+        String output = lineBreak + "\n\n";
+        output += "Username: " + username + "\n\n";
+        output += "Bio: " + bio + "\n\n";
+
+        for (int i = 0; i < posts.size(); i++) { //display a list of posts created by the user
+            output += posts.get(i).toString() + "\n";
+        }
+
+        for (int i = 0; i < posts.size(); i++) { //display a list of comments created by the user
+            output += posts.get(i).toString() + "\n";
+        }
+
+        output += lineBreak + "\n";
+        return output;
+    }
+    
+   //toFile function
+    public String[] toFile() {
+    	ArrayList<String> output = new ArrayList<String>();
+    	output.add("Profile");
+    	output.add(username);
+    	output.add(password);
+    	output.add("bio:" + bio); //We need to have bio: here, if its empty, parse issues
+    	return output.toArray(new String[0]);
+    }
+    
+    // Generates a random username or password
+    public void computerGenerateName(String select) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println("Select 1 to generate a unique name or" +
+                " select any other number to keep name: ");
+
+        int generate = scan.nextInt();
+        scan.nextLine();
+
+        try {
+            if (generate != 1) {
+                System.out.println(select);
+            } else if (generate == 1) {
+                // Formulate a randomized username unique to user
+                String characters = "!>/$%^&*#@+=:";
+                
+                // Generate a random integer from 0 - 300000000
+                int randomize = rand.nextInt(300000000);
+
+                int randomize2 = rand.nextInt(characters.length());
+                int randomize3 = rand.nextInt(characters.length());
+                int randomize4 = rand.nextInt(characters.length());
+                
+                // Loop through private static names list 
+                String name = names[(int) (Math.random() * names.length)];
+                
+                // Indexes characters string and appoints string in index to a character
+                char randChar = characters.charAt(randomize2);
+                char randChar2 = characters.charAt(randomize3);
+                char randChar3 = characters.charAt(randomize4);
+
+                // Create unique username 
+                if (select.equals(getUsername())) {
+                    setUsername("user" + randomize + randChar + randChar2 + randChar3);
+                    System.out.println(getUsername());
+                // Create unique password    
+                } else if (select.equals(getPassword())) {
+                    setPassword(name + randomize + randChar + randChar2 + randChar3);
+                    System.out.println(getPassword());
+                }
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            System.out.println("Unable to randomize!");
+        }
+    }
+
+    //TODO: Remove me, for testing
+    public String getPassword() {
+        return password;
+    }
+
+/*    //Make sure the password matches through the encryption
+    public boolean verifyPass(String input) {
+        input = cryptoHashFunction(input);
+        return input.equals(password);
+    }*/
+
+    //Set user bio, user can set their bio
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    //Add a post to the account (move this into Application.java, since it's a basic function?)
+    public void addPost(Post post) {
+        posts.add(post); //newest posts for this account go the back
+/*            Date newPostTime = post.getTimeStamp();
+            boolean added = false; //if user is adding a post, it would be the most recent???
+            for (int i = 0; i < posts.size(); i++) {
+                Date timeStamp = posts.get(i).getTimeStamp();
+                //Returns an array formatted like this:
+                //int[year,day,hour,minute]
+
+                if (newPostTime.compareTo(timeStamp) > 0) {
+                    posts.add(i, post);
+                    i = posts.size();
+                    added = true;
+                    continue;
+                }
+            }
+
+            if (!added) {
+                posts.add(post);
+            }*/
+    }
+
+    public void deletePost(Post post) {
+        //thinking about putting this in Application
+        //ALSO: addPost, deleteAccount and editPost
+    }
+
+    //Set the username
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    //Set the password (password should be encrypted in Application.java)
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
