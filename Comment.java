3e08b9602959285708c25d99337884287dec029e
jmaduafo <87540591+jmaduafo@users.noreply.github.com>
2021-07-19 03:04:49 +0100

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -23,6 +23,22 @@ public class Comment {
     public Date getTimestamp() {
         return timestamp;
     }
+     
+    // Edit however you like
+    public void editComment(Post editor, String content, String newComment) {
+        String regex = "(.*)" + content + "(.*)";
+
+        try {
+            if (editor.getAuthor().equals(editor.getAuthor())) {
+                String edit = content.replaceAll(regex, newComment);
+                System.out.println(edit);
+            }
+        } catch(Exception e) {
+            System.out.println("Author not found! Comment wasn't able to edit");
+        } finally {
+            System.out.println("Try-catch done!");
+        }
+        }
 
     public String toString() {
         String output = "";
