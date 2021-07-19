1fffa92649749415ca43eae3cd4e496871244d20
athdot <athdot@gmail.com>
2021-07-19 16:51:44 -0400

    Data integration part one
    Uncomment the test case main function to get an idea of what happens

--- Application.java
+++ Application.java
@@ -94,31 +94,7 @@ public class Application {
     //note: change all input to be String so program doesn't break if non-int is entered? (?do later)
     //nvm, instructions say application should not crash under any circumstances
 
-    //This function takes a password and encrypts it, just for funsies
-    public String cryptoHashFunction(String password) {
-        //Will always be 32 digits, add digits
-        if (password.length() > 32) {
-            password = password.substring(0, 32);
-        } else {
-            for (int i = password.length(); i <= 32; i++) {
-                password = "0" + password;
-            }
-        }
-
-        int divisor =  0;
-        for (int i = 0; i < password.length(); i++) {
-            divisor += (int) password.charAt(i);
-        }
-
-        String output = "";
-        // char 32 to 126 range
-        for (int i = 0; i < password.length(); i ++) {
-            output += (char) ((((int) password.charAt(i)) * 1000 * i / divisor) % 93 + 33);
-        }
-
-        return output;
-    }
-
+  
     public void login() { //control the login section of the program
 
         boolean validCredentials = false;
@@ -174,10 +150,10 @@ public class Application {
                         //create array of length 2, contains username, then encrypted password
                         String[] userPass = line.split(" ");
                         if (userPass[0].equalsIgnoreCase(username)
-                                && userPass[1].equals(cryptoHashFunction(password))) {
+                                && userPass[1].equals(CryptoHash.getHash(password))) {
                             validCredentials = true;
                             //assign Account to current instance of Application
-                            user = new Account(username, cryptoHashFunction(password));
+                            user = new Account(username, CryptoHash.getHash(password));
                             break;
                         }
                     }
@@ -236,7 +212,7 @@ public class Application {
                         FileOutputStream fos = new FileOutputStream(f, true);
                         PrintWriter pw = new PrintWriter(fos);
 
-                        pw.println(username + " " + cryptoHashFunction(password)); //write new account to file
+                        pw.println(username + " " + CryptoHash.getHash(password)); //write new account to file
                         System.out.println(accountCreated);
                         pw.close();
 
@@ -273,7 +249,7 @@ public class Application {
                 System.out.println(currentPasswordPrompt);
                 String password = scanner.nextLine();
 
-                if (cryptoHashFunction(password).equals(user.getPassword())) {
+                if (CryptoHash.getHash(password).equals(user.getPassword())) {
                     //prompt for new password
                     System.out.println(newPasswordPrompt);
                     password = scanner.nextLine();
