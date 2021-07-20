da6f8dd83cae475f27d51b4c42e1bbe02312658e
jmaduafo <87540591+jmaduafo@users.noreply.github.com>
2021-07-20 04:38:32 +0100

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -30,64 +30,6 @@ public class Comment {
         return timestamp;
     }
     
-    public void editComment(Post editor, String author, String newContent) {
-        //
-        String regex = "(.*)" + editor.getContent() + "(.*)";
-
-        try {
-            if (editor.getAuthor().equals(author)) {
-                String editComment = editor.getContent().replaceAll(regex, newContent);
-                editor.setContent(editComment);
-                System.out.println(editor.getContent());
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-            System.out.println("Author not found! Comment wasn't able to edit");
-        } finally {
-            System.out.print("");
-        }
-        }
-
-    public void editAuthor(Post editor, String author, String newAuthor) {
-        // Encases old author name to be replaced with the new author
-        String regex = "(.*)" + editor.getAuthor() + "(.*)";
-
-        try {
-            // Makes sure that only author can edit
-            if (editor.getAuthor().equals(author)) {
-                // Replace original author name with the new author name;
-                String editAuthor = editor.getAuthor().replaceAll(regex, newAuthor);
-                editor.setAuthor(editAuthor);
-                System.out.println(editor.getAuthor());
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-            System.out.println("Author not found! Author wasn't able to edit");
-        } finally {
-            System.out.print("");
-        }
-    }
-    
-
-    public void editTitle(Post editor, String author, String newTitle) {
-        // Encases old title to be replaced with the new title
-        String regex = "(.*)" + editor.getTitle() + "(.*)";
-
-        try {
-            // Makes sure that only author can edit
-            if (editor.getAuthor().equals(author)) {
-                // Replaces old title with new title
-                String editTitle = editor.getTitle().replaceAll(regex, newTitle);
-                editor.setTitle(editTitle);
-                System.out.println(editor.getTitle());
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-            System.out.println("Author not found! Title wasn't able to edit");
-        } finally {
-            System.out.print("");
-        }
-    }
     
     public String toFile() {
     	String output = "";
