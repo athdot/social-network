050970e75fddd98412280cd7b78af782e4df944f
jmaduafo <87540591+jmaduafo@users.noreply.github.com>
2021-07-20 04:35:35 +0100

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -48,18 +48,42 @@ public class Comment {
         }
         }
 
+    public void editAuthor(Post editor, String author, String newAuthor) {
+        // Encases old author name to be replaced with the new author
+        String regex = "(.*)" + editor.getAuthor() + "(.*)";
+
+        try {
+            // Makes sure that only author can edit
+            if (editor.getAuthor().equals(author)) {
+                // Replace original author name with the new author name;
+                String editAuthor = editor.getAuthor().replaceAll(regex, newAuthor);
+                editor.setAuthor(editAuthor);
+                System.out.println(editor.getAuthor());
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("Author not found! Author wasn't able to edit");
+        } finally {
+            System.out.print("");
+        }
+    }
+    
+
     public void editTitle(Post editor, String author, String newTitle) {
+        // Encases old title to be replaced with the new title
         String regex = "(.*)" + editor.getTitle() + "(.*)";
 
         try {
+            // Makes sure that only author can edit
             if (editor.getAuthor().equals(author)) {
+                // Replaces old title with new title
                 String editTitle = editor.getTitle().replaceAll(regex, newTitle);
                 editor.setTitle(editTitle);
                 System.out.println(editor.getTitle());
             }
         } catch(Exception e) {
             e.printStackTrace();
-            System.out.println("Title not found! Title wasn't able to edit");
+            System.out.println("Author not found! Title wasn't able to edit");
         } finally {
             System.out.print("");
         }
