002e7a0d83d09ab6ec355097c9d32259b0e52c65
athdot <athdot@gmail.com>
2021-07-21 15:58:24 -0400

    No crashes

--- Application.java
+++ Application.java
@@ -1020,6 +1020,12 @@ public class Application {
 
     public static void main(String[] args) {
         Application app = new Application();
-        app.start(); //run the program
+        try {
+        	app.start(); //run the program
+        } catch (Exception e) {
+        	//Catch any other misc errors
+        	System.out.println("An Error Occured");
+        	app.start();
+        }
     }
 }
