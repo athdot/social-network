13c18cd2ca5bef2f6ceda4d0d0cb2d8b7b6e3c34
jmaduafo <87540591+jmaduafo@users.noreply.github.com>
2021-07-21 20:44:09 +0100

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -56,23 +56,42 @@ public class Comment {
         return output;
     }
     
-    public void emojis(String[] emoticon) {
+    public void emojis() {
+        String[] emojis = {"\uD83D\uDE00","\uD83D\uDE01","\uD83D\uDE03","\uD83D\uDE04",
+                "\uD83D\uDE17","\uD83D\uDE18","\uD83D\uDE09","\uD83D\uDE1C", "\uD83E\uDD29","\uD83E\uDD2A",
+                "\uD83E\uDD23","\uD83D\uDE22","\uD83D\uDE2D","\uD83E\uDD70","\uD83D\uDE0D","\uD83D\uDE1B",
+                "\uD83D\uDE1C","\uD83D\uDC45","\uD83E\uDD11","\uD83E\uDD14","\uD83E\uDD10","\uD83D\uDE10",
+                "\uD83D\uDE05","\uD83D\uDE26","\uD83D\uDE2E","\uD83D\uDE30","\uD83D\uDE36","\uD83D\uDE3A",
+                "\uD83E\uDD2C","\uD83E\uDD2E","\uD83E\uDD74","\uD83D\uDE0E","\uD83D\uDE0F","\uD83D\uDE14",
+                "\uD83D\uDE15","\uD83D\uDE16"};
+
         System.out.println("Emoji drop down menu: ");
 
         int bambi = 1;
-        for (String i : emoticon) {
+        for (String i : emojis) {
             System.out.print((bambi++) + "." + " " + i + " ");
         }
+        System.out.println();
+
     }
 
-//    public String emojiSelection(String[] emoticon) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Select emoji number: ");
-//
-//        int emoji = scanner.nextInt();
-//        scanner.nextLine();
-//
-//        return emoticon[emoji - 1];
-//    }
+    public String emojiSelection() {
+        String[] emojis = {"\uD83D\uDE00","\uD83D\uDE01","\uD83D\uDE03","\uD83D\uDE04",
+                "\uD83D\uDE17","\uD83D\uDE18","\uD83D\uDE09","\uD83D\uDE1C", "\uD83E\uDD29","\uD83E\uDD2A",
+                "\uD83E\uDD23","\uD83D\uDE22","\uD83D\uDE2D","\uD83E\uDD70","\uD83D\uDE0D","\uD83D\uDE1B",
+                "\uD83D\uDE1C","\uD83D\uDC45","\uD83E\uDD11","\uD83E\uDD14","\uD83E\uDD10","\uD83D\uDE10",
+                "\uD83D\uDE05","\uD83D\uDE26","\uD83D\uDE2E","\uD83D\uDE30","\uD83D\uDE36","\uD83D\uDE3A",
+                "\uD83E\uDD2C","\uD83E\uDD2E","\uD83E\uDD74","\uD83D\uDE0E","\uD83D\uDE0F","\uD83D\uDE14",
+                "\uD83D\uDE15","\uD83D\uDE16"};
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Select emoji number: ");
+
+        int emoji = scanner.nextInt();
+        scanner.nextLine();
+
+        return emojis[emoji - 1];
+
+    }
 }
